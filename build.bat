f96ff51bf701f1dcca81727445df90dd8d4cd1d1
qiuxs <1162796928@qq.com>
2018-05-18 21:44:57 +0800

    修改批处理

--- build.bat
+++ build.bat
@@ -1,6 +1,14 @@
 @echo off
-mvn -DskipTests=true clean package
-mvn dependency:copy-dependencies -DoutputDirectory=target\lib
+echo build start
+call mvn -DskipTests=true clean package
+echo copy-dependencies to target\lib
+call mvn dependency:copy-dependencies -DoutputDirectory=target\lib
 cd target
 mkdir cute-generate
-cp .\lib cute-generate
+cp -rf .\lib cute-generate
+::set jarName=ls -S | grep cute-code-generate -m 1
+::ls -S | grep cute-code-generate -m 1
+::echo %jarName%
+::echo "jarName = " + %jarName%
+::cp -rf %jarName% .\cute-generate
+cd ../
